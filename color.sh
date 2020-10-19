20693b3e598cd50ebf30bd3c4000d600d6071f74
Ben Roberts <benjamin.roberts01@mymail.champlain.edu>
2020-10-19 17:01:53 -0400

    Updated colors and output

--- color.sh
+++ color.sh
@@ -5,7 +5,7 @@ green='\033[0;32m'
 yellow='\033[0;33m'
 blue='\033[0;34m'
 magenta='\033[0;35m'
-default=='\033[0m'
+default='\033[0m'
 
 read -p "Which is your favorite color? Red, green, blue, yellow, or magenta: " color
 color=${color^^}
@@ -16,13 +16,22 @@ case $color in
 			;;
 	"RED")		echo "$color's selected"
 			selected_color=$red
-			exit
 			;;
 	"BLUE")		echo "$color's selected"
 			selected_color=$blue
-			exit
+			;;
+	"YELLOW")	echo "$color's selected"
+			selected_color=$yellow
+			;;
+	"PURPLE")	echo "$color's selected"
+			selected_color=$magenta
+			;;
+	"GREEN")	echo "$color's selected"
+			selected_color=$green
 			;;
 	*)		echo "Some other color was selected"
 			exit 1
 			;;
 esac
+
+echo -e ${selected_color}Your selected color is ${color}${default}
